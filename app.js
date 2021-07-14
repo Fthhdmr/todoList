277a10705bef1face4c4435623107bb629b3d710
fthhdmr@gmail.com <fthdmr@gmail.com>
2021-07-14 11:27:43 +0300

    update-js

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ const getTodosFromStorage = () => {
     return (storage) ? storage : [];
 }
 
+const getDonesFromStorage = () => {
+    const storage = JSON.parse (localStorage.getItem('dones'));
+    return (storage) ? storage : [];
+}
+
 const todos = getTodosFromStorage();
+const dones = getTodosFromStorage();
 
 const getTodosToPage = () => {
     todos.forEach((todo) => {
@@ -44,6 +50,56 @@ const removeTodoFromStorage = (todo) => {
         todos.splice(index,1);
         localStorage.setItem('todos',JSON.stringify(todos));
     }
+}
+
+const removeDoneFromStorage = (done) => {
+    const index = dones.indexOf(done);
+    if(index > -1){
+        dones.splice(index,1);
+        localStorage.setItem('dones',JSON.stringify(dones));
+    }
+}
+
+const checkTodo = (target) => {
+    const todo = target.parentNode.childNodes[0].innerHTML;
+    moveTodoToDone(todo,target);
+}
+
+const moveTodoToDone = (todo,target) => {
+    removeTodoFromStorage(todo);
+    dones.push(todo);
+    localStorage.setItem('dones',JSON.stringify(dones));
+    makeItDone(target);
+}
+
+ const moveDoneToTodos = (done,target) => {
+     removeDoneFromStorage(done);
+     todos.push(done);
+     localStorage.setItem('todos',JSON.stringify(todos));
+     makeItTodo(target);
+ }
+
+const makeItDone = (target) => {
+    const done = target.parentNode.classList.add('done');
+    target.parentNode.classList.remove('todo');
+    target.parentNode.childNodes[2].setAttribute('onclick','removeDone(this)');
+    target.className = '';
+    target.classList.add('fas','fa-check-square');
+    target.setAttribute('onclick','uncheckDone(this)');
+}
+
+const makeItTodo = (target) => {
+    target.parentNode.classList.remove('done');
+    target.parentNode.classList.add('todo');
+    target.parentNode.childNodes[2].setAttribute('onclick','removeTodo(this)');
+    target.className = '';
+    target.classList.add('fas','fa-square');
+    target.setAttribute('onclick','checkTodo(this)');
+}
+
+const uncheckDone = (target) => {
+    const done = target.parentNode.childNodes[0].innerHTML;
+    moveDoneToTodos(done,target);
 }
 
 const createTodoItem = (text) => {
